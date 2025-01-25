dcebe2ac43bc5662774e4b5914329abe20b5035b
facerom <78068016+facerom@users.noreply.github.com>
2025-01-25 12:48:30 -0500

    Add netlify.toml for deployment configuration

---
+++ tetris-react/components/Board.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Board: React.FC = () => {
+    return (
+        <div>
+            <h3>Tetris Game Board</h3>
+            {/* Board rendering logic will go here */}
+        </div>
+    );
+};
+
+export default Board;
---
+++ tetris-react/components/Tetris.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tetris: React.FC = () => {
+    return (
+        <div>
+            <h2>Tetris Game Component</h2>
+            {/* Game board and logic will go here */}
+        </div>
+    );
+};
+
+export default Tetris;
